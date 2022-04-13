f018f5a6f81423cd92be7f60cf4ae9f8dbb47667
Nitai Chandra Banik <nitai.banik@selise.ch>
2022-04-13 23:02:43 +0600

    fix launch settings and controller

--- Controllers/PingController.cs
+++ Controllers/PingController.cs
@@ -4,18 +4,12 @@ namespace RepositoryPattern.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class PingController
+    : ControllerBase
 {
-    private readonly ILogger<PingController> _logger;
-
-    public WeatherForecastController(ILogger<PingController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "Ping")]
+    [HttpGet("/ping", Name = "Ping")]
     public ActionResult<string> Get()
     {
-	return Ok("Pong");
+        return Ok("Pong");
     }
 }
