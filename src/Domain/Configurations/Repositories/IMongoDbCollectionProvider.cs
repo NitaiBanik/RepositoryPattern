2a01608aa369d541fc5ee771219e61cef08a0518
Nitai Chandra Banik <nitai.banik@selise.ch>
2022-04-14 00:32:39 +0600

    repository creating with database settings

---
+++ src/Domain/Configurations/Repositories/IMongoDbCollectionProvider.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Driver;
+
+namespace Domain.Configurations.Repositories;
+
+public interface IMongoDbCollectionProvider
+{
+    IMongoCollection<T> GetDbColection<T>()
+        where T : AggregateRoot;
+}
---
+++ src/Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Configurations.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services)
+    {
+        services.AddSingleton<IMongoDbCollectionProvider, IMongoDbCollectionProvider>();
+
+        return services;
+    }
+
+}
